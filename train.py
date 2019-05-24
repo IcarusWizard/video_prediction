c93236856c45ba47e35ccd0ee074954e42185b6c
Icarus <wizardicarus@gmail.com>
2019-05-24 13:52:01 +0800

    add val

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
 import torch
 import torchvision
+import numpy as np
 
 from Video_dataset import VideoDataset
 from model_util import setup_seed, mse_loss, save_model, load_model
+from data_util import torch_save_gif
 from models import CDNA
 
 import argparse, os
@@ -29,7 +31,7 @@ def main():
     train_set = VideoDataset(args.data_path, 'train', args.horizon, fix_start=False)
     val_set = VideoDataset(args.data_path, 'val', args.horizon, fix_start=True)
 
-    config = dict(train_set.get_config())
+    config = train_set.get_config()
     H, W, C = config['observations']
     A = config['actions'][0]
     T = args.horizon
@@ -48,7 +50,6 @@ def main():
     epoch = args.start_point
     while epoch < args.start_point + args.epoch:
         for j, data in enumerate(train_loader):
-            steps += 1
             observations = data['observations']
             actions = data['actions']
 
@@ -58,7 +59,7 @@ def main():
 
             predicted_observations = model(observations[0], actions)
 
-            loss = mse_loss(observations, predicted_observations)
+            loss = mse_loss(observations, predicted_observations) / args.batch_size
             loss.backward()
             torch.nn.utils.clip_grad_norm_(model.parameters(), 10)
 
@@ -67,6 +68,34 @@ def main():
 
         epoch += 1
         save_model(model, os.path.join(args.model_path, '{}_{}.pt'.format(args.model_name, epoch)))
+
+        gif_path = os.path.join(args.model_path, args.model_name, 'val_{}'.format(epoch))
+        if not os.path.exists(gif_path):
+            os.makedirs(gif_path)
+
+        losses = []
+
+        for j, data in enumerate(val_loader):
+            observations = data['observations']
+            actions = data['actions']
+
+            # B x T ==> T x B
+            observations = torch.transpose(observations, 0, 1)
+            actions = torch.transpose(actions, 0, 1)
+
+            predicted_observations = model(observations[0], actions)
+
+            video = torch.cat([observations[0, 0].unsqueeze(0), predicted_observations[0 : T - 1, 0]])
+            torch_save_gif(os.path.join(gif_path, "{}.gif".format(j)), video, fps=10)
+
+            loss = mse_loss(observations, predicted_observations).item() / args.batch_size
+            losses.append(loss)
+            
+            opt.zero_grad()
+
+        print("-" * 50)
+        print("In epoch {}, loss in val set is {}".format(epoch, np.mean(losses)))
+        print("-" * 50)
 
 
 if __name__ == '__main__':
