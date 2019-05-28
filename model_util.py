3c6c2d2d42463758711a244d4598a16b540e41cf
IcarusWizard <loststart@foxmail.com>
2019-05-28 20:17:38 +0800

    add apply_cdna_kernel

--- model_util.py
+++ model_util.py
@@ -1,4 +1,5 @@
 import torch
+from torch.functional import F
 import numpy as np
 
 import random
@@ -21,3 +22,32 @@ def load_model(model, filename, eval_mode=False):
     model.load_state_dict(torch.load(filename))
     if eval_mode:
         model.eval()
+
+def apply_cdna_kernel(image, kernel):
+    """
+        Inputs:
+            image -> tensor[B, C, H, W]
+            kernal -> tensor[B, N, K, K]
+        Outputs:
+            new_image -> tensor[B, N, C, H, W]
+    """
+    batch_size = image.shape[0]
+    image_channel = image.shape[1]
+    image_height = image.shape[2]
+    image_width = image.shape[3]
+    num_kernel = kernel.shape[1]
+    kernel_size = kernel.shape[2]
+    padding = kernel_size // 2
+
+    _image = image.transpose(0, 1) # [C, B, H, W]
+    _kernel = kernel.view(batch_size * num_kernel, 1, kernel_size, kernel_size) # [B * N, 1, K, K]
+
+    output = F.conv2d(_image, _kernel, stride=1, padding=padding, groups=batch_size) # [C, B * N, H, W]
+
+    output = output.view(image_channel, batch_size, num_kernel, image_height, image_width) # [C, B, N, H, W]
+
+    return output.permute(1, 2, 0, 3, 4) # [B, N, C, H, W]
+
+
+
+
